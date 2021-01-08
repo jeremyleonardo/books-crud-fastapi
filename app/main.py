eebab362e88ce162423669dbace07f6cc67b8c7e
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2021-01-08 12:03:26 +0700

    add offset for get books

--- app/main.py
+++ app/main.py
@@ -6,6 +6,12 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime, date
 from typing import Optional
 
+
+# DISCLAIMER:
+# This is a very simple CRUD API
+# Not intended for production
+
+
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
@@ -17,9 +23,11 @@ recreate_database()
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    return {"message": "Hello"}
+    return {"message": "Sample books API is online"}
+
 
 @app.get("/books/{id}")
 async def find_book(id: int):
@@ -30,14 +38,18 @@ async def find_book(id: int):
     session.close()
     return {"book": book}
 
+
 @app.get("/books")
-async def get_books(limit: int = 10):
-    if(limit > 100 | limit < 0):
-        limit = 100
+async def get_books(page_size: int = 10, page: int = 1):
+    if(page_size > 100 or page_size < 0):
+        page_size = 100
+
+    page -= 1
     session = Session()
-    books = session.query(Book).limit(limit).all()
+    books = session.query(Book).limit(page_size).offset(page*page_size).all()
     session.close()
     return {"books": books}
+
 
 @app.post("/books")
 async def create_book(title: str, pages: int):
@@ -54,6 +66,7 @@ async def create_book(title: str, pages: int):
         "message": "success"
     }
 
+
 @app.put("/books")
 async def update_book(id: int, title: str, pages: int):
     session = Session()
@@ -65,6 +78,7 @@ async def update_book(id: int, title: str, pages: int):
     return {
         "message": "success"
     }
+
 
 @app.delete("/books")
 async def delete_book(id: int):
